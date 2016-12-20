091cb55234e2be52fadb90700a1870a5c6b9eeae
Le Hieu <hieuletrung307@gmail.com>
2016-12-20 15:01:01 +0700

    asdad

---
+++ app.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+
+var express = require('express')
+var app = express()
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
+
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+});
+
+
