51891b2abdb585fdd937c24035f332065e7b14de
Le Hieu <hieuletrung307@gmail.com>
2016-12-20 15:15:47 +0700

    asdsad
    asdasd

--- app.js
+++ app.js
@@ -10,8 +10,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
+
 
 
